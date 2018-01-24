c120f8d8d701dcd818a608e515a4af794c5b695d
Matias Grioni <matgrioni@gmail.com>
2018-01-23 23:32:01 -0500

    Updated demo.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pprint
-
 # Synopsis from Wikipedia.
 LOCATIONS = {
     "Seattle": {
@@ -12,6 +10,7 @@ LOCATIONS = {
         ],
         "synopsis": "Seattle is a seaport city on the west coast of the "
                     "United States.",
+        "budget": 5000
     },
     "Boston": {
         "attributes": [
@@ -22,6 +21,7 @@ LOCATIONS = {
         "synopsis": "Boston is the capital city and most populous "
                     "municipality of the Commonwealth of Massachusetts "
                     "in the United States.",
+        "budget": 700
     },
     "Columbus": {
         "attributes": [
@@ -30,29 +30,122 @@ LOCATIONS = {
         ],
         "synopsis": "Columbus is the state capital and the most populous "
                     "city of the U.S. state of Ohio.",
+        "budget": 200
+    },
+    "Washington DC": {
+        "attributes": [
+            "AMERICAN_HISTORY",
+            "MUSEUMS",
+            "POLITICS",
+        ],
+        "synopsis": "Washington, D.C., the capital of the United States of "
+                    "America and the seat of its three branches of government, "
+                    "has an unparalleled collection of free, public museums, and "
+                    "the lion's share of the nation's most treasured monuments "
+                    "and memorials. The vistas on the National Mall between the "
+                    "Capitol, Washington Monument, White House, and Lincoln Memorial "
+                    "are iconic throughout the world.",
+        "budget": 2500
+    },
+    "Cuba": {
+        "attributes": [
+            "SPANISH",
+            "HISTORIC",
+            "TROPICAL",
+            "CIGARS",
+            "RUM",
+            "BEACHES",
+        ],
+        "synopsis": "Cuba is the largest Caribbean island, between the Caribbean Sea "
+                    "and the North Atlantic Ocean. It lies 145 km (90 miles) south of Key West, "
+                    "Florida, between the Cayman Islands and the Bahamas, to the west of Haiti, "
+                    "and northwest of Jamaica.",
+        "budget": 1000
     },
 }
 
 
 def main():
-    print("Welcome to JourneyBoiye")
+    print("Welcome to JourneyBoiye\n")
 
-    # Prompt for user's name.
+    # Prompt for basic info.
     name = input("Name: ")
-    print("Welcome {}!".format(name))
-
-    # Prompt for what they do/don't like.
-    likes = input("What kinds of things do you like?\n")
-
-    # Prompt for current zip-code.
+    salary = input("Salary: ")
+    budget = 5000
+    age = input("Age: ")
     zipCode = input("What's your zip-code? ")
 
-    print("Given your profile, you may be interested in:")
+    likes = input("Tell me about yourself and interests:")
+    print()
 
-    # We're going to hardcode some search criteria.
-    suggestions = dict(filter(lambda x: "SEAFOOD" in x[1]["attributes"], LOCATIONS.items()))
+    print("Welcome {}!".format(name))
+    print()
 
-    pprint.pprint(suggestions)
+    print("Now, I'm going to ask you where you've been before and what you thought of these places")
+    another = ""
+    while another != "NO":
+        loc = input("Location: ")
+        rating = input("Rating (1-5): ")
+        comments = input("Comments: ")
+        another = input("Another (NO to quit)? ")
+        print()
+
+
+    print("Now enter some commands!")
+    print()
+
+    cmd = ""
+    while cmd != "QUIT":
+        cmd = input("> ").upper()
+        if "SALARY" in cmd:
+            budget = 1000
+        elif "BROWSE" in cmd or "SHOW" in cmd:
+            print("Recommendations")
+            print()
+            suggestions = dict(filter(lambda x: x[1]["budget"] == budget, LOCATIONS.items()))
+
+            for name, attrs in suggestions.items():
+                print(name)
+                print(attrs["synopsis"])
+                print()
+                print("Recommended because\n")
+
+                if budget == 5000:
+                    print("Seafood")
+                    print("CSE Major")
+                    print("Large Salary\n")
+
+                    print("Here's what Seattle has to offer")
+                    for attr in attrs["attributes"]:
+                        print(attr)
+
+                elif budget == 1000:
+                    print("Spanish Culture")
+                    print("Lower salary\n")
+
+                    print("Here's what Cuba has to offer")
+                    for attr in attrs["attributes"]:
+                        print(attr)
+
+            print()
+            response = input("Thoughts on the above? ")
+            print("Thank you for your input")
+
+        elif "FIND" in cmd or "SEARCH" in cmd:
+            suggestions = dict(filter(lambda x: "AMERICAN_HISTORY" in x[1]["attributes"], LOCATIONS.items()))
+            print("Results")
+            print()
+
+            for name, attrs in suggestions.items():
+                print("Here's what {} has to offer".format(name))
+                print(attrs["synopsis"])
+                print()
+
+                for attr in attrs["attributes"]:
+                    print(attr)
+                print()
+
+        print()
 
 if __name__ == "__main__":
     main()
